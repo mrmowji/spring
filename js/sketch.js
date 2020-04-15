082f7118cc3e4a22958983c12193bae55ce93baf
Mojtaba Javan <javan.mojtaba@gmail.com>
2020-04-15 15:03:23 +0430

    Change new clouds trigger threshold

--- js/sketch.js
+++ js/sketch.js
@@ -90,10 +90,9 @@ function updateClouds() {
   let numberOfNewClouds = 0;
   for (let i = 0; i < clouds.length; i++) {
     clouds[i].x--;
-    if (clouds[i].x == canvasWidth / 2 - 10) {
+    if (clouds[i].x == Math.floor(canvasWidth / 2) - 10) {
       numberOfNewClouds++;
     }
-
   }
   for (let i = clouds.length - 1; i >= 0; i--) {
     if (clouds[i].x < (-cloudImages[clouds[i].imageIndex].width - 10)) {
